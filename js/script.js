81acc1554c6ad07c62895f8a74db40bca8448fd0
Gackun1 <gaku090707@gmail.com>
2021-11-18 18:00:59 +0900

    add loading

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
+const hamburger = () => {
+  document.getElementById("line1").classList.toggle("line1_active");
+  document.getElementById("line2").classList.toggle("line2_active");
+  document.getElementById("line3").classList.toggle("line3_active");
+  document.getElementById("header").classList.toggle("header--open");
+  document
+    .getElementById("hamburger_button")
+    .classList.toggle("hamburger_button--open");
+  document
+    .getElementById("content-wrapper")
+    .classList.toggle("content-wrapper--full");
+};
+document.getElementById("hamburger_button").addEventListener("click", () => {
+  hamburger();
+});
+
 document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = () => {
-    document.getElementById("line1").classList.toggle("line1_active");
-    document.getElementById("line2").classList.toggle("line2_active");
-    document.getElementById("line3").classList.toggle("line3_active");
-    document.getElementById("header").classList.toggle("header--open");
-  };
-  document.getElementById("hamburger_button").addEventListener("click", function () {
+  if (document.body.offsetWidth <= 480) {
     hamburger();
-  });
+  }
+
+  const loading = document.getElementById("loading");
+  const loadingSvg = document.getElementById("loading-svg");
+  new Vivus(
+    "loading-svg",
+    {
+      start: "autostart",
+      type: "delayed",
+      duration: 150,
+      animTimingFunction: Vivus.EASE,
+    },
+    () => {
+      loading.classList.add("loading--loaded");
+    }
+  );
 });
